fc6487bbe30f3fe747f7d7796c85a484b93e190a
MadimetjaMadix <madimetjasethosa@gmail.com>
2020-04-26 14:43:17 +0200

    interacting with web elements using their ID

--- LServer/index.js
+++ LServer/index.js
@@ -4,5 +4,13 @@ button.addEventListener('click', function () {
   const paragraph = document.createElement('p') // Create <p> element
   const text = document.createTextNode('This is a student') // Create text node
   paragraph.appendChild(text) // Append the text to <p>
-  document.body.appendChild(paragraph) // Append <p> to <body>
+  const studentList = document.getElementById('studentList')
+  studentList.appendChild(paragraph) // Append <p> to <body>
+}, false)
+
+const button1 = document.getElementById('myButton')
+
+button1.addEventListener('click', function () {
+  const headerElement = document.getElementById('heading')
+  headerElement.innerHTML = 'My New Heading'
 }, false)
